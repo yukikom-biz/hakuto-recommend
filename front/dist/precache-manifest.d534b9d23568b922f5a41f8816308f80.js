07967bea33a8c3f582e934d04564da3621fc8a5d
yukikom-biz <yuki.komatsu@bizreach.co.jp>
2019-09-25 18:01:40 +0900

    git for deploy

---
+++ front/dist/precache-manifest.d534b9d23568b922f5a41f8816308f80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c8c4af0cf6a8ecedd6e1",
+    "url": "/js/chunk-vendors.1eaf3f97.js"
+  },
+  {
+    "revision": "ce62015bcba8a9bbdebb",
+    "url": "/js/app.e2af8604.js"
+  },
+  {
+    "revision": "86bc64abfdd27e8b702d",
+    "url": "/js/about.faaf6b38.js"
+  },
+  {
+    "revision": "ee1306297a98cc631fe513a6ae987524",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ce62015bcba8a9bbdebb",
+    "url": "/css/app.f34889a4.css"
+  }
+];
